89b03b16e33bb4e4832b9188ef163d5383852df2
saikiranyalavarthi <yalavarthisaikiran3482@gmail.com>
2023-06-30 21:38:47 +0530

    first commit

---
+++ src/LoginForm.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Perform API login request
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        // Login successful
+        setError('');
+        // Do something with the login token or redirect to another page
+      } else {
+        // Login failed
+        const data = await response.json();
+        setError(data.message || 'Login failed');
+      }
+    } catch (error) {
+      setError('An error occurred during login');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      {error && <p className="error-message">{error}</p>}
+    </div>
+  );
+};
+
+export default LoginForm;
